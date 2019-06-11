e542763c8a83fe528d2197d8105ecc7711b828d3
Thomas Nelis <tomnelis53@gmail.com>
2019-06-11 15:04:05 +0200

    some gh pages stuff

--- src/Components/BMICategories.js
+++ src/Components/BMICategories.js
@@ -13,8 +13,14 @@ const BMICategories = (BMI) => {
         return "Obese Class I (Moderately obese)"
     } else if (BMI >= 35 && BMI < 40) {
         return "Obese Class II (Severely obese)"
-    } else {
-        return "You're too damn big"
+    } else if (BMI >= 40 && BMI < 45) {
+        return "Obese Class III (Very severely obese)"
+    } else if (BMI >= 45 && BMI < 50) {
+        return "Obese Class IV (Morbidly Obese)"
+    } else if (BMI >= 50 && BMI < 60) {
+        return "Obese Class V (Super Obese)"
+    } else if (BMI >= 60) {
+        return "Obese Class VI (Hyper Obese)"
     }
 }
 
